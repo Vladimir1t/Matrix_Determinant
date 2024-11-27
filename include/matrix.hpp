890bbc9cfacc07c2da65b95426b81c7d4048e7b1
Vladimir1t <vova.vexov@mail.ru>
2024-11-27 21:29:24 +0300

    add base class matrix

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -9,29 +9,36 @@
 
 namespace Matrix {
 
-// template<class elem_t>
-// class matrix {
-
-// private:
-
-//     size_t rows_ = 0;
-//     size_t col_  = 0;
-
-
-// };
-
 template<class elem_t>
 class matrix {
 
 private:
+    size_t rows_ = 0;
+    size_t col_  = 0;
 
-    size_t size_ = 0;
-    elem_t det_  = 0;
+protected:
+    elem_t det_;
     my::buffer<elem_t> buffer_;
 
 public:
+
+    matrix(size_t rows, size_t col): rows_(rows), col_(col) {}
+
+    virtual void print() = 0;
+};
+
+template<class elem_t>
+class matrix_sq : public matrix<elem_t> {
+
+private:
+
+    using matrix<elem_t>::buffer_;
+    using matrix<elem_t>::det_;
+    size_t size_ = 0;
+
+public:
     
-    matrix(size_t size) {
+    matrix_sq(size_t size) : matrix<elem_t>(size, size) {
         if (size <= 0)
             throw std::invalid_argument("size <= 0");
         size_ = size;
@@ -43,7 +50,7 @@ public:
         buffer_.push_elem(new_elem);
     }
 
-    void print() {
+    void print() override {
         for (int i = 0; i < size_; ++i) {
             for (int j = 0; j < size_; ++j) {
                 std::cout << buffer_[i][j] << ' ';
